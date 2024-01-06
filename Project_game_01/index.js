9579e3ee7805fdcb4af117e4466a94b1055eb74e
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-06 10:49:04 +0700

    game 00 DOne

---
+++ Project_game_01/index.js
@@ -0,0 +1,40 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+
+
+
+document.addEventListener('keypress',function(event){
+    console.log(event.key);
+    if (event.key == 'j'){
+
+        if(!character.classList.contains("animation")){
+            character.classList.add('animation');
+        }
+        // console.log("ini J")
+        setTimeout(function() {
+            character.classList.remove('animation');
+        }, 500);
+    }
+})
+
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        // block.style.animation = "none";
+        // block.style.display = "none";
+        alert("You lose.");
+    }
+}, 10);
+
+
+// function jump(){
+//     if (character.classList != "animation"){
+//         character.classList.add('animation');
+//     }
+//     setTimeout(function(){
+//         character.classList.remove('animation');
+//     },500);
+// }
